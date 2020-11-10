842592c7e6731ee7f1acfcda482c822959652195
Jan Skowronski <crianonim@gmail.com>
2020-11-10 08:22:17 +0100

    display all dice

--- roller/src/Main.elm
+++ roller/src/Main.elm
@@ -3,7 +3,7 @@ module Main exposing (main)
 import Browser
 import Dict
 import Html exposing (Attribute, Html, a, button, div, h1, hr, img, input, p, pre, text)
-import Html.Attributes exposing (selected, src, value)
+import Html.Attributes exposing (selected, src, type_, value)
 import Html.Events exposing (onClick, onInput)
 import Platform.Cmd exposing (Cmd)
 import Random
@@ -21,7 +21,7 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { result = 1, count=3 , dice=[1]}
+    ( { result = 1, count = 3, dice = [ 1 ] }
     , Cmd.none
     )
 
@@ -33,20 +33,20 @@ update msg model =
             ( model, Random.generate NewDie (Random.list model.count (Random.int 1 6)) )
 
         NewDie x ->
-            ( { model | result = List.foldl (+) 0 x }, Cmd.none )
-        ChangeCount s->
-            ( {model | count = Maybe.withDefault 1 (String.toInt s)},Cmd.none)
+            ( { model | result = List.foldl (+) 0 x, dice = x }, Cmd.none )
+
+        ChangeCount s ->
+            ( { model | count = Maybe.withDefault 1 (String.toInt s) }, Cmd.none )
 
 
 type alias Model =
-    {  result: Int, count:Int, dice:List Int}
+    { result : Int, count : Int, dice : List Int }
 
 
 type Msg
     = Roll
     | NewDie (List Int)
     | ChangeCount String
-
 
 
 
@@ -65,11 +65,15 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ div [] [
-            button [onClick Roll] [text "Roll"]
-            , input [onInput ChangeCount
-            ,value (String.fromInt model.count)] []
-            
-        ]
+        [ div []
+            [ button [ onClick Roll ] [ text "Roll" ]
+            , input
+                [ onInput ChangeCount
+                , value (String.fromInt model.count)
+                , type_ "number"
+                ]
+                []
+            ]
         , p [] [ text (String.fromInt model.result) ]
+        , p [] [ text (String.join "+" (List.map String.fromInt model.dice)) ]
         ]
