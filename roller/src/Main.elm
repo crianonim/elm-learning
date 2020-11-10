b455cc633cfa6e363301b8c2d39cceb5e3a9ebea
Jan Skowronski <crianonim@gmail.com>
2020-11-10 07:59:27 +0100

    custom number of dice

--- roller/src/Main.elm
+++ roller/src/Main.elm
@@ -21,8 +21,8 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { die = 1 }
-    , Random.generate NewDie (Random.int 1 6)
+    ( { result = 1, count=3 , dice=[1]}
+    , Cmd.none
     )
 
 
@@ -30,19 +30,23 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Roll ->
-            ( model, Random.generate NewDie (Random.int 1 6) )
+            ( model, Random.generate NewDie (Random.list model.count (Random.int 1 6)) )
 
         NewDie x ->
-            ( { model | die = x }, Cmd.none )
+            ( { model | result = List.foldl (+) 0 x }, Cmd.none )
+        ChangeCount s->
+            ( {model | count = Maybe.withDefault 1 (String.toInt s)},Cmd.none)
 
 
 type alias Model =
-    { die : Int }
+    {  result: Int, count:Int, dice:List Int}
 
 
 type Msg
     = Roll
-    | NewDie Int
+    | NewDie (List Int)
+    | ChangeCount String
+
 
 
 
@@ -63,6 +67,9 @@ view model =
     div []
         [ div [] [
             button [onClick Roll] [text "Roll"]
+            , input [onInput ChangeCount
+            ,value (String.fromInt model.count)] []
+            
         ]
-        , p [] [ text (String.fromInt model.die) ]
+        , p [] [ text (String.fromInt model.result) ]
         ]
